7a32e269187d627b39f6fcbc493d5356479d7c40
sam <samuel13241@gmail.com>
2024-01-10 18:50:29 -0800

    no message

---
+++ src/main/java/tinfo/project/tinfo482/Tinfo482Application.java
@@ -0,0 +1,13 @@
+package tinfo.project.tinfo482;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tinfo482Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tinfo482Application.class, args);
+	}
+
+}
---
+++ src/test/java/tinfo/project/tinfo482/Tinfo482ApplicationTests.java
@@ -0,0 +1,13 @@
+package tinfo.project.tinfo482;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tinfo482ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
