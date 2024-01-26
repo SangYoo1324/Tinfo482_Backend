49f6201d766ef811f93a4edf7f660d97f8931283
sam <samuel13241@gmail.com>
2024-01-25 22:08:26 -0800

    allowed origin *

--- src/main/java/tinfo/project/tinfo482/config/CustomCorsFilter.java
+++ src/main/java/tinfo/project/tinfo482/config/CustomCorsFilter.java
@@ -30,7 +30,7 @@ public class CustomCorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", allowedOrigin );
+        response.setHeader("Access-Control-Allow-Origin", "*" );
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
